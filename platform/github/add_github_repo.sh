5fb024aae77427a37290ef60a3c42588c9f97572
rsanjeevan <ramanathan.sanjeevan@davinta.com>
2018-02-22 15:11:35 +0530

    DIDA-1 For verifying github-jira intgration, updated script to create github repo

--- platform/github/add_github_repo.sh
+++ platform/github/add_github_repo.sh
@@ -9,13 +9,13 @@ if [ "${GITHUB_USER}" == "" ]; then
   export GITHUB_USER="ciuserr"
 fi
 
-if [ "${GITHUB_PASSWORD}" == "" ]; then
+if [ "${GITHUB_PASSWD}" == "" ]; then
   echo "Please ensure Github password is set for $GITHUB_USER that is configured. Will exit now"
   exit 1
 fi
 
-CHECK_REPO="curl -X GET -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/repos/davinta/$1"
-CREATE_REPO="curl -u $GITHUB_USER:$GITHUB_PASSWORD -d '{ \"name\": \"$1\", \"auto_init\": true, \"private\": true }' https://api.github.com/orgs/davinta/repos"
+CHECK_REPO="curl -s -X GET -u $GITHUB_USER:$GITHUB_PASSWD https://api.github.com/repos/davinta/$1"
+CREATE_REPO="curl -s -u $GITHUB_USER:$GITHUB_PASSWD -d '{ \"name\": \"$1\", \"auto_init\": true, \"private\": true }' https://api.github.com/orgs/davinta/repos"
 
 if [ "$(eval $CHECK_REPO | grep message)" != "" ]; then
   eval $CREATE_REPO
@@ -23,7 +23,6 @@ else
   echo "Repo $1 exists or verification failed"
 fi
 
-ROOT_DIR="$HOME"
 git clone https://github.com/davinta/$1.git
 cd $1
 for i in develop feature; do echo $i; git branch $i; git checkout $i; git push origin $i; done
